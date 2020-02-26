8d80f1a20755df7c566b5fa70b7455d3d8b34c3f
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2020-02-26 17:16:41 +0100

    chore: backend initial state

---
+++ backend/warehouse/src/main/java/cloud/cholewa/warehouse/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
+
+}
